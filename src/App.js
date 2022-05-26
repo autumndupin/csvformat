d5fd83acd9b25e73dcc4930e992830df99270429
“Autumn <autumn@tpn.health>
2022-05-25 22:22:48 -0400

    allow user to download csv

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import React, { useState } from "react";
+import FileSaver from 'file-saver';
 import Papa from 'papaparse';
 import {transformData} from './helpers'
 
 function App() {
   const [parsedCSV, setParsedCSV] = useState(); 
+  const [dupeOption, setDupeOption] = useState("email");
 
-  const handleOnChange = (e) => {
+  const handleUpload = (e) => {
     handleParse(e.target.files[0])
   };
+
+  const handleSelect = (e) => {
+    setDupeOption(e.target.value)
+  }
 
   const handleParse = (file) => {
       Papa.parse(file, {
@@ -19,24 +25,37 @@ function App() {
       });
   }
 
+  const handleExport = (csv) => {
+    const csvData = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    FileSaver.saveAs(csvData, 'data.csv');
+  }
+
   const handleOnSubmit = (e) => {
-    transformData(parsedCSV)
+    const newCSV = transformData(parsedCSV, dupeOption)
+    handleExport(newCSV)
+    debugger;
   }
 
   return (
     <div>
       <form>
-      <input
-          type={"file"}
-          id={"csvImport"}
-          accept={".csv"}
-          onChange={handleOnChange}
-        />
+        <input
+            type={"file"}
+            id={"csvImport"}
+            accept={".csv"}
+            onChange={handleUpload}
+          />
+        <select id="dupeSelect" name="dupeOptions" onChange={handleSelect}>
+          <option value="email">Remove duplicate rows by email</option>
+          <option value="phone">Remove duplicate rows by phone</option>
+          <option value="emailOrPhone">Remove duplicate rows by email or phone</option>
+        </select>
         <button
           onClick={(e) => {
             handleOnSubmit(e);
           }}
-        >Import CSV</button>
+          >Import CSV
+        </button>
       </form>
     </div>
   );
