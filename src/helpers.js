5741a0ee055a65fc8fa5877120bb1cff7aba7b2b
“Autumn <autumn@tpn.health>
2022-05-25 22:23:05 -0400

    filter by dupe options and generate csv

--- src/helpers.js
+++ src/helpers.js
@@ -1,20 +1,56 @@
+import Papa from 'papaparse';
+
+// takes in a parsed CSV file (array of objects) and returns new CSV without duplicate rows
 export function transformData(parsedCSV, option) {
-  console.log(parsedCSV)
-  filterByEmailDupes(parsedCSV)
+  let filteredRows = []
+
+  if (option === "email") {
+
+    filteredRows = filterByEmailDupes(parsedCSV)
+    const transformedData = generateNewCSV(filteredRows) 
+    return transformedData
+
+  } else if (option === "phone") {
+
+    filteredRows = filterByPhoneDupes(parsedCSV)
+    const transformedData = generateNewCSV(filteredRows) 
+    return transformedData
+
+  } else if (option === "emailOrPhone") {
+
+    filteredRows = filterByEmailOrPhoneDupes(parsedCSV)
+    const transformedData = generateNewCSV(filteredRows) 
+    return transformedData
+
+  }
 }
 
+// takes in a parsed CSV file (array of objects) and returns only unique rows filtered by email
 const filterByEmailDupes = (parsedCSV) => {
   const filteredRows = parsedCSV.filter((row, index, array) => array.findIndex(r => r.Email === row.Email) === index);
   console.log(filteredRows)
   return filteredRows
 }
 
+// takes in a parsed CSV file (array of objects) and returns only unique rows filtered by phone
 const filterByPhoneDupes = (parsedCSV) => {
   const filteredRows = parsedCSV.filter((row, index, array) => array.findIndex(r => r.Phone === row.Phone) === index);
   console.log(filteredRows)
 }
 
+// takes in a parsed CSV file (array of objects) and returns only unique rows filtered by email or phone
 const filterByEmailOrPhoneDupes = (parsedCSV) => {
   const filteredRows = parsedCSV.filter((row, index, array) => array.findIndex(r => r.Email === row.Email || r.Phone === row.Phone) === index);
   console.log(filteredRows)
+}
+
+// takes in a set of filtered rows (array of objects) and returns a new csv file
+const generateNewCSV = (filteredRows) => {
+  const header = ["First Name", "Last Name", "Email", "Phone"]
+  const newCSV = Papa.unparse({
+    "fields": header,
+    "data": filteredRows
+  });
+
+  return newCSV;
 }
