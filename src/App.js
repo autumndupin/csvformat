e49ec30664e0d93311da58fa4d5760d045aaeb61
“Autumn <autumn@tpn.health>
2022-05-25 21:36:12 -0400

    set up app csv import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import './App.css';
+import React, { useState } from "react";
+import Papa from 'papaparse';
+import {transformData} from './helpers'
 
 function App() {
+  const [parsedCSV, setParsedCSV] = useState(); 
+
+  const handleOnChange = (e) => {
+    handleParse(e.target.files[0])
+  };
+
+  const handleParse = (file) => {
+      Papa.parse(file, {
+        header: true,
+        complete: (results) => {
+          setParsedCSV(results.data)
+        },
+      });
+  }
+
+  const handleOnSubmit = (e) => {
+    transformData(parsedCSV)
+  }
+
   return (
     <div>
       <form>
-        <input type={"file"} accept={".csv"} />
-        <button>Import CSV</button>
+      <input
+          type={"file"}
+          id={"csvImport"}
+          accept={".csv"}
+          onChange={handleOnChange}
+        />
+        <button
+          onClick={(e) => {
+            handleOnSubmit(e);
+          }}
+        >Import CSV</button>
       </form>
     </div>
   );
